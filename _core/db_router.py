256c06f1c0149e7450e1609596ebe8f676af9238
SylverVB <victorelish@gmail.com>
2024-08-06 01:56:01 -0400

    WIP: Add RDS connection and update settings

---
+++ _core/db_router.py
@@ -0,0 +1,15 @@
+class PrimaryReplicaRouter:
+    def db_for_read(self, model, **hints):
+        return 'default'
+
+    def db_for_write(self, model, **hints):
+        return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        db_list = ('default', 'fallback')
+        if obj1._state.db in db_list and obj2._state.db in db_list:
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        return db == 'default'
