26df146308829b0bdf0fa52d4b1be5748c0ad2b1
andyeshelman <andy.eshelman@gmail.com>
2024-08-08 13:05:54 -0400

    add table and a few endpoints to test Plaid connectivity

--- _core/settings.py
+++ _core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'expenses',
     'accounts',
+    'plaids',
 ]
 
 MIDDLEWARE = [
---
+++ plaids/api/routes.py
@@ -0,0 +1,52 @@
+from ninja import Router
+from django.http import HttpRequest, JsonResponse
+from plaid.model.item_public_token_exchange_request import ItemPublicTokenExchangeRequest
+from plaid.model.transactions_sync_request import TransactionsSyncRequest
+from plaid.model.products import Products
+from plaid.model.sandbox_public_token_create_request import SandboxPublicTokenCreateRequest
+
+from plugins.plaid import client
+from plaids.models import Plaid
+from util.schemas import Token
+
+router = Router(tags=['plaid'])
+
+@router.post('/sandbox_public_token', response={200: Token})
+def sandbox_public_token(request: HttpRequest, institution_id: str):
+    pt_request = SandboxPublicTokenCreateRequest(
+        institution_id=institution_id,
+        initial_products=[Products('transactions')]
+    )
+    pt_response = client.sandbox_public_token_create(pt_request)
+    return Token(pt_response['public_token'])
+
+@router.post('/gen_access_token')
+def gen_access_token(request: HttpRequest, public_token: Token):
+    exchange_request = ItemPublicTokenExchangeRequest(
+        public_token=public_token.token
+    )
+    exchange_response = client.item_public_token_exchange(exchange_request)
+    Plaid.objects.create(
+        access_token=exchange_response['access_token'],
+        item_id=exchange_response['item_id'],
+        user=request.user
+    )
+    
+@router.get('/transactions')
+def get_transactions(request: HttpRequest):
+    transactions = []
+    for item in request.user.plaids.all():
+        access_token = item.access_token
+        req = TransactionsSyncRequest(
+            access_token=access_token,
+        )
+        response = client.transactions_sync(req)
+        transactions += response['added']
+        while (response['has_more']):
+            req = TransactionsSyncRequest(
+                access_token=access_token,
+                cursor=response['next_cursor']
+            )
+            response = client.transactions_sync(req).to_dict()
+            transactions += response['added']
+    return JsonResponse({'transactions': [t.to_dict() for t in transactions]})
