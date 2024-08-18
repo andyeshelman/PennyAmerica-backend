3eb49b84fc22de9804bf371684eb4d11183b65a1
andyeshelman <andy.eshelman@gmail.com>
2024-08-18 01:29:22 -0400

    CORS hack

--- _core/settings.py
+++ _core/settings.py
@@ -31,7 +31,8 @@ PRODUCTION = os.getenv('PRODUCTION')
 
 if PRODUCTION:
     ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-    CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
+    CORS_ALLOW_ALL_ORIGINS = True
+    #CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
 else:
     DEBUG = True
     CORS_ALLOWED_ORIGINS = ['http://localhost:5173',]
